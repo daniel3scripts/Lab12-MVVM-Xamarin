691d8be42f6e77ea3561b23157a3611788a3b2f4
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-06-11 23:59:20 -0500

    commit01

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab12MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new View.Calculator();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ViewModel/OperatorCalcu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lab12MVVM.ViewModel
+{
+    public class OperatorCalcu:ViewModelBase
+    {
+        public static double Calculate(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "×":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+        }
+
+    }
+    
+	
+}
